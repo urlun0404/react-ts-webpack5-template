024acef5685bba68857f3c96b2c6bd1dac448dd0
Lauren Lin <urlun0404@gmail.com>
2022-11-02 22:58:52 +0800

    Separate babel config out from webpack config

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -29,13 +29,6 @@ module.exports = {
         exclude: '/node-modules/',
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              ['@babel/preset-react', { runtime: 'automatic' }],
-              '@babel/preset-typescript',
-            ],
-          },
         },
       },
       {
